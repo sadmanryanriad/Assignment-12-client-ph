fd0cc4d5cb79615aa569ff7f030d0e5654086e0a
sadmanryanriad <sadmanryanriad@gmail.com>
2023-12-04 20:06:19 +0600

    more month added

--- src/Pages/dashboard/progress/Progress.jsx
+++ src/Pages/dashboard/progress/Progress.jsx
@@ -58,6 +58,16 @@ const Progress = () => {
             <option value="">All</option>
             <option value="January">January</option>
             <option value="February">February</option>
+            <option value="March">March</option>
+            <option value="April">April</option>
+            <option value="May">May</option>
+            <option value="June">June</option>
+            <option value="July">July</option>
+            <option value="August">August</option>
+            <option value="September">September</option>
+            <option value="October">October</option>
+            <option value="November">November</option>
+            <option value="December">December</option>
           </select>
         </div>
       </form>
