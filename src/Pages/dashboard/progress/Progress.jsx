110c035d96a057113eabd59e03c87599b23d689a
sadmanryanriad <sadmanryanriad@gmail.com>
2023-12-04 19:32:23 +0600

    sort in the progress

--- src/Pages/dashboard/progress/Progress.jsx
+++ src/Pages/dashboard/progress/Progress.jsx
@@ -5,26 +5,65 @@ import Spinner from "../../../components/Spinner";
 import WorkSheetTable from "../../../components/WorkSheetTable";
 
 const Progress = () => {
-    const [workSheetData, setWorkSheetData] = useState([]);
-    const [isloading, setIsLoading] = useState(true);
-    const axiosSecure = useAxiosSecure();
+  const [workSheetData, setWorkSheetData] = useState([]);
+  const [isloading, setIsLoading] = useState(true);
+  const [sortName, setSortName] = useState("");
+  const [sortMonth, setSortMonth] = useState("");
+  const axiosSecure = useAxiosSecure();
 
-    useEffect(()=>{
-        axiosSecure.get(`/work-sheet`)
-        .then(res=>{
-            setWorkSheetData(res.data);
-            setIsLoading(false);
-        })
-    },[axiosSecure])
-
-    if(isloading) return <Spinner></Spinner>
+  useEffect(() => {
+    let apiUrl = `/work-sheet/${sortName}`;
     
-    return (
-        <div>
-            <Heading>Work Sheets</Heading>
-            <WorkSheetTable data={workSheetData}></WorkSheetTable>
+    // If a month is selected, add it to the API URL
+    if (sortMonth) {
+      apiUrl += `/${sortMonth}`;
+    }
+
+    axiosSecure.get(apiUrl).then((res) => {
+      setWorkSheetData(res.data);
+      setIsLoading(false);
+    });
+  }, [axiosSecure, sortName, sortMonth]);
+
+  const handleSortedName = (e) => {
+    setSortName(e.target.value);
+  };
+
+  const handleSortedMonth = (e) => {
+    setSortMonth(e.target.value);
+  };
+
+  if (isloading) return <Spinner></Spinner>;
+
+  return (
+    <div>
+      <Heading>Work Sheets</Heading>
+      <form className="mb-8 max-w-fit mx-auto">
+        <h2 className="text-xl font-semibold mb-2">Sort by:</h2>
+        <div className="flex gap-2 items-center justify-center">
+          <label>Name:</label>
+          <select
+            className="select select-bordered w-full max-w-xs dark:bg-gray-700"
+            onChange={handleSortedName}
+          >
+            <option value="">All</option>
+            <option value="talha@gmail.com">talha</option>
+            <option value="baby@gmail.com">baby</option>
+          </select>
+          <label>Month:</label>
+          <select
+            className="select select-bordered w-full max-w-xs dark:bg-gray-700"
+            onChange={handleSortedMonth}
+          >
+            <option value="">All</option>
+            <option value="January">January</option>
+            <option value="February">February</option>
+          </select>
         </div>
-    );
+      </form>
+      <WorkSheetTable data={workSheetData}></WorkSheetTable>
+    </div>
+  );
 };
 
 export default Progress;
